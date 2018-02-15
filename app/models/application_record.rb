056b11ba33c00dc05a98eaa6cd1bba9b29bb318b
G. Zentkovich <gzentkovich@gmail.com>
2018-02-14 23:21:25 -1000

    bulk commit

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  after_initialize do
-  end
   self.abstract_class = true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  # model associations
+  # model association
   belongs_to :todo
 
-  # validation
+  # validations
   validates_presence_of :name
 end
